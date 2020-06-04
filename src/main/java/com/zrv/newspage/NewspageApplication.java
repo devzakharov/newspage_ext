76f05a2aba0defe581262abaf33fe69ffdf1565e
devzakharov <dev.zakharov@gmail.com>
2020-06-04 11:59:55 +0300

    Initial commit

---
+++ src/main/java/com/zrv/newspage/NewspageApplication.java
@@ -0,0 +1,14 @@
+package com.zrv.newspage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspageApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NewspageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zrv/newspage/NewspageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrv.newspage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
