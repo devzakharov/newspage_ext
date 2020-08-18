f8f484f7cbc79f4bb096e56cb12a4f8b8b180c3b
devzakharov <dev.zakharov@gmail.com>
2020-08-18 17:11:04 +0300

    some fixes

--- src/main/java/com/zrv/newspage/controller/UserRegistrationController.java
+++ src/main/java/com/zrv/newspage/controller/UserRegistrationController.java
@@ -1,6 +1,8 @@
 package com.zrv.newspage.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.zrv.newspage.domain.User;
 import com.zrv.newspage.exception.WrongUserDataException;
 
@@ -12,6 +14,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Set;
@@ -22,7 +25,7 @@ public class UserRegistrationController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-            handleRequest(req, resp);
+        handleRequest(req, resp);
     }
 
     @Override
@@ -38,14 +41,28 @@ public class UserRegistrationController extends HttpServlet {
 
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-
+        setAccessControlHeaders(resp);
         PrintWriter output = resp.getWriter();
 
-        String login = req.getParameter("login");
-        String password = req.getParameter("password");
-        String email = req.getParameter("email");
+        StringBuffer stringBuffer = new StringBuffer();
+        String line;
+
+        try {
+            BufferedReader reader = req.getReader();
+            while ((line = reader.readLine()) != null)
+                stringBuffer.append(line);
+        } catch (Exception e) { /*report an error*/ }
+
+        // TODO: Switch JSON library to Jackson
+        JsonObject jsonObject = new JsonParser().parse(stringBuffer.toString()).getAsJsonObject();
+
+        String login = jsonObject.get("login").getAsString();
+        String password = jsonObject.get("password").getAsString();
+        String email = jsonObject.get("email").getAsString();
 
         User user = new User(login, email, password);
+
+        System.out.println(user.toString());
 
         String json;
 
@@ -57,15 +74,31 @@ public class UserRegistrationController extends HttpServlet {
 
             resp.setStatus(HttpServletResponse.SC_OK);
 
-            json = "{status:'success'}";
+            json = "{\"status\":\"success\"}";
 
         } catch (WrongUserDataException e) {
 
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 
+            // TODO: Switch JSON library to Jackson
             json = new Gson().toJson(e.getErrorsList());
         }
 
         output.write(json);
+    }
+
+    //for Preflight
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        setAccessControlHeaders(resp);
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    private void setAccessControlHeaders(HttpServletResponse resp) {
+        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        resp.setHeader("Access-Control-Max-Age", "1000");
+        resp.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, accept, x-access-token");
     }
 }
--- src/main/java/com/zrv/newspage/exception/WrongUserDataException.java
+++ src/main/java/com/zrv/newspage/exception/WrongUserDataException.java
@@ -2,7 +2,7 @@ package com.zrv.newspage.exception;
 
 import java.util.List;
 
-public class WrongUserDataException extends RuntimeException {
+public class WrongUserDataException extends Exception {
     List<String> errorsList;
 
     public WrongUserDataException(List<String> list) {
